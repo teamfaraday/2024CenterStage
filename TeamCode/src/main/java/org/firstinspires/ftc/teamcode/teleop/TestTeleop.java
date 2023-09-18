1e826e7d3aa93cf9e37aa6de5405c06e83a534fa
Devam Mondal <devammondal06@gmail.com>
2023-09-18 18:22:48 -0400

    test teleop for drive train complete

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TestTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TestTeleop.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.helper.HardwareSetup;
 
@@ -24,11 +25,23 @@ public class TestTeleop extends OpMode {
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backLeft");
-
     }
 
     @Override
     public void loop() {
+
+        double minPower = -0.8;
+        double maxPower = 0.8;
+
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x * 1.25;
+        double rx = gamepad1.right_stick_x * 0.5;
+
+        frontLeft.setPower(Range.clip(y +x +rx,minPower,maxPower));
+        backLeft.setPower(Range.clip(y -x +rx,minPower,maxPower));
+        frontRight.setPower(Range.clip(y -x -rx,minPower,maxPower));
+        backRight.setPower(Range.clip(y +x -rx,minPower,maxPower));
+
 
     }
 }
